a50ea3478737c2be1dfa2df046359fd07003311b
Chester <88622982+ChesterLZW@users.noreply.github.com>
2026-03-24 19:32:39 +0800

    godspeed

--- app/static/config.js
+++ app/static/config.js
@@ -1,7 +1,7 @@
 var CONFIG = {
     canvas: { width: 480, height: 640 },
 
-    player: { width: 60, height: 65, speed: 100 },
+    player: { width: 200, height: 300, speed: 10000 },
 
     startingLives: 3,
 
